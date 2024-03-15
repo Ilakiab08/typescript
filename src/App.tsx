148f23ba354d2e8fd68cf895585b34d78b0d7fb1
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-03-15 10:56:13 +0100

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
 
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-       
-    </>
-  )
+    <div className="app">
+      <header>
+        <h1>Welcome to My React App</h1>
+      </header>
+      <main>
+        <p>This is the content of my React application.</p>
+        <p>I am Trying  Typescript <code>App.tsx</code> file to customize my app.</p>
+      </main>
+      <footer>
+        <p>Copyright &copy; {new Date().getFullYear()} My React App</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
